347ed168afb696546a0bfc106a4ec1cfd03eb854
Stavros Kollias <kollias1252@gmail.com>
2020-10-19 20:32:09 +0100

    did Navigation

--- dist/js/navigationHandlers.js
+++ dist/js/navigationHandlers.js
@@ -1,33 +1,39 @@
 const ulItems = document.querySelectorAll(".side-bar-item");
 
 ulItems.forEach((element, i) => {
-  const btn = element.children[0].querySelector("a");
-  const submenu = element.querySelector(".side-bar-item-list");
-  btn.addEventListener("click", (e) => {
-    if (submenu.classList.length == 1) {
+   const btn = element.children[0].querySelector("a");
+   const submenu = element.querySelector(".side-bar-item-list");
+   btn.addEventListener("click", (e) => {
       const activeSideMenu = document.querySelector(".active-side-1");
-      if (activeSideMenu) activeSideMenu.classList.remove("active-side-1");
-      btn.querySelector(".fa-angle-down").className = "fas fa-angle-up";
-      submenu.classList.add("active-side-1");
-    } else {
-      btn.querySelector(".fa-angle-up").className = "fas fa-angle-down";
-      submenu.classList.remove("active-side-1");
-    }
-  });
-
-  const subMenuSub = submenu.querySelectorAll("ul");
-  subMenuSub.forEach((element, i) => {
-    const subBtnSub = element.parentElement.querySelector("a");
-    subBtnSub.addEventListener("click", (e) => {
-      if (element.classList.length == 1) {
-        const activeSideMenu2 = document.querySelector(".active-side-2");
-        if (activeSideMenu2) activeSideMenu.classList.remove("active-side-2");
-        subBtnSub.querySelector(".fa-angle-down").className = "fas fa-angle-up";
-        element.classList.add("active-side-2");
+      const activeBtn = document.querySelector(".current");
+      if (submenu.classList.length == 1) {
+         if (activeSideMenu) activeSideMenu.classList.remove("active-side-1");
+         if (activeBtn) activeBtn.classList.remove("current");
+         btn.classList.add("current");
+         submenu.classList.add("active-side-1");
       } else {
-        subBtnSub.querySelector(".fa-angle-up").className = "fas fa-angle-down";
-        element.classList.remove("active-side-2");
+         submenu.classList.remove("active-side-1");
+         btn.classList.remove("current");
       }
-    });
-  });
+   });
+
+   if (submenu) {
+      const subMenuSub = submenu.querySelectorAll("ul");
+      subMenuSub.forEach((element, i) => {
+         const subBtnSub = element.parentElement.querySelector("a");
+         subBtnSub.addEventListener("click", (e) => {
+            const activeSideMenu2 = document.querySelector(".active-side-2");
+            if (element.classList.length == 0) {
+               const activeBtn = document.querySelector(".current-2");
+               if (activeSideMenu2) activeSideMenu.classList.remove("active-side-2");
+               if (activeBtn) activeBtn.classList.remove("current-2");
+               subBtnSub.classList.add("current-2");
+               element.classList.add("active-side-2");
+            } else {
+               subBtnSub.classList.remove("current-2");
+               element.classList.remove("active-side-2");
+            }
+         });
+      });
+   }
 });
